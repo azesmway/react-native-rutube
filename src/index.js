f0b15359f8c7252b6a79800820b108c851ca7886
azesm <azesm@me.com>
2022-04-07 10:49:10 +0300

    fix script

--- src/index.js
+++ src/index.js
@@ -44,11 +44,11 @@ const RutubeView = ({
       },
     });
 
-    if (videoData) {
-      setVideo(videoData);
+    if (videoData && videoData.result) {
+      setVideo(videoData.result);
 
-      if (videoData.video && videoData.video.embed_url) {
-        setPlayUrl(videoData.video.embed_url);
+      if (videoData.result.video && videoData.result.video.embed_url) {
+        setPlayUrl(videoData.result.video.embed_url);
       }
     }
   };
@@ -93,6 +93,46 @@ const RutubeView = ({
     );
   };
 
+  const MAIN_SCRIPT = () => {
+    return `<!DOCTYPE html>
+<html>
+  <head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    >
+    <style>
+      body {
+        margin: 0;
+      }
+      .container {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 56.25%;
+      }
+      .video {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <iframe
+      width="${width}"
+      height="${height}"
+      src="${playUrl}"
+      frameborder="0"
+      allowfullscreen
+      allow="encrypted-media">
+    </iframe>
+  </body>
+</html>`;
+  };
+
   return (
     <View style={{ height, width }}>
       <WebView
@@ -115,7 +155,7 @@ const RutubeView = ({
             : ''
         }
         {...webViewProps}
-        source={playUrl}
+        source={MAIN_SCRIPT()}
         ref={webViewRef}
         onMessage={onWebMessage}
       />
